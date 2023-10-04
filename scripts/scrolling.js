be7eeeb4f5c3297ed317906e2b8ab335cf37fd5d
Enes Gündüz <56505397+Deaglis1197@users.noreply.github.com>
2023-10-04 21:34:17 +0300

    Update: scrolling onclick buttons

---
+++ scripts/scrolling.js
@@ -0,0 +1,21 @@
+document.documentElement.style.scrollBehavior = "smooth";
+let sectionButtons = document.getElementById("section-buttons").children;
+let resumeSections = document.querySelectorAll(".resume-section");
+
+setButtons();
+
+function setButtons() {
+  for (let button of sectionButtons) {
+    let section;
+    for (let resume of resumeSections) {
+      if (resume.id == button.id) {
+        section = resume;
+        break;
+      }
+    }
+    console.log(section);
+    button.addEventListener("click", () => {
+      section.scrollIntoView();
+    });
+  }
+}
